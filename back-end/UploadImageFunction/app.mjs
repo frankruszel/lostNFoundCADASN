b217c529a88014ef9ba7db310b5aadb87f915d91
frankruszel <110735562+frankruszel@users.noreply.github.com>
2025-02-02 07:34:29 +0800

    add multipart

--- back-end/UploadImageFunction/app.mjs
+++ back-end/UploadImageFunction/app.mjs
@@ -1,73 +1,84 @@
 import AWS from 'aws-sdk';
 import { v4 as uuidv4 } from 'uuid'; // UUID library for generating unique IDs
-import parser from 'lambda-multipart-parser';
-const s3 = new AWS.S3();
-const bucketName = 'prod-lostnfound-store-item-images';
 
-// Function to insert data into S3
-const uploadImageToS3 = async (file) => {
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const tableName = 'ItemTable';
 
-    console.log({file})
-    let params = {
-      Bucket: bucketName,
-      Key: file.filename,// key
-      Body: file.content
-    }
-    const savedFile = s3.putObject(params).promise();
-    // await dynamoDB.put(params).promise();
-    console.log(`Uploaded image to S3`);
-    return savedFile
+// Function to insert data into DynamoDB
+const insertItemDataIntoDynamoDB = async (itemData) => {
+  try {
+    const params = {
+      TableName: tableName,
+      Item: itemData,
+    };
 
+    await dynamoDB.put(params).promise();
+    console.log(`Inserted item data into DynamoDB: ${JSON.stringify(itemData)}`);
+  } catch (error) {
+    console.error('Error inserting item data into DynamoDB:', error);
+    throw new Error('Failed to insert item data into DynamoDB.');
+  }
 };
 
 export const lambdaHandler = async (event, context) => {
   console.log('Received event:', JSON.stringify(event));
   console.log('Lambda context:', JSON.stringify(context));
 
-
-  // try {
-  //   // Use event directly if it's already an object, otherwise parse event.body
-  //   requestBody = typeof event === 'object' && event.item ? event : JSON.parse(event.body);
-  // } catch (error) {
-  //   console.error('Invalid JSON in event or event.body:', event.body || event);
-  //   return {
-  //     statusCode: 400,
-  //     headers: {
-  //       "Access-Control-Allow-Origin": "*", 
-  //       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  //       "Access-Control-Allow-Headers": "Content-Type, Authorization", 
-  //     },
-  //     body: JSON.stringify({
-  //       message: 'Invalid JSON format in request body.',
-  //     }),
-  //   };
-  // }
+  let requestBody;
 
   try {
-    const { files } = await parser.parse(event);
-    const filesData = files.map(uploadImageToS3);
-    const results = await Promise.allSettled(filesData);
-    // let userId = null
-    // if ("userId" in requestBody){
-    //   userId = requestBody.userId
-    // }
-    // // console.log(userId,budgets, typeof(budgets) === 'object')
-    // // Validate input
-    // if ( !userId || !file ) {
-    //   return {
-    //     statusCode: 400,
-    //     body: JSON.stringify({
-    //       message: 'Missing or invalid userId, file, or item in request body.',
-    //     }),
-    //   };
-    // }
-    
-  
-    // Upload into S3
-    // let file = requestBody.body
-    // let fileName = file.filename;
-    // let fileContent = file.content;
-    // await uploadImageToS3(userId,fileName,fileContent);
+    // Use event directly if it's already an object, otherwise parse event.body
+    requestBody = typeof event === 'object' && event.item ? event : JSON.parse(event.body);
+  } catch (error) {
+    console.error('Invalid JSON in event or event.body:', event.body || event);
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*", 
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization", 
+      },
+      body: JSON.stringify({
+        message: 'Invalid JSON format in request body.',
+      }),
+    };
+  }
+
+  try {
+    const { userId, item, file } = requestBody;
+    // console.log(userId,budgets, typeof(budgets) === 'object')
+    // Validate input
+    if ( !userId || typeof(item) !== 'object' || !file ) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: 'Missing or invalid userId, file, or item in request body.',
+        }),
+      };
+    }
+
+    // Generate UUID for item and process rooms
+    const itemId = uuidv4();
+
+    const s3ImageKey = `${userId}/${file.filename}`;
+
+
+    const itemData = {
+      userId,
+      itemId:itemId,
+      title: item.title,
+      description: item.description,
+      image_url: item.image_url,
+      itemStatus: item.itemStatus, // lost (someone is finding this), found (someone has found a lost item), claimed (someone claimed a lost item)
+      category: item.category,
+      dateFound: new Date(item.dateFound).toISOString(),
+      dateClaimed: item.dateClaimed != null ? new Date(item.dateClaimed).toISOString() : null,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    };
+
+    // Insert into DynamoDB
+    await insertItemDataIntoDynamoDB(itemData);
 
     return {
       statusCode: 200,
@@ -76,20 +87,18 @@ export const lambdaHandler = async (event, context) => {
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, Authorization", 
       },
-      body: results
+      body: JSON.stringify({
+        message: 'item data successfully created and stored in DynamoDB.',
+        itemData,
+      }),
     };
   } catch (error) {
     console.error('Error processing item data:', error);
     return {
       statusCode: 500,
-      headers: {
-        "Access-Control-Allow-Origin": "*", 
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization", 
-      },
       body: JSON.stringify({
         message: 'An error occurred while processing the request.',
-        error: requestBody,
+        error: error.message,
       }),
     };
   }
