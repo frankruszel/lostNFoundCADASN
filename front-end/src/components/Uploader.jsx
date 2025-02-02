38c3dcd13ac52cc00b70870d977fa39296e81584
frankruszel <110735562+frankruszel@users.noreply.github.com>
2025-02-02 12:15:38 +0800

    add rekognition role

--- front-end/src/components/Uploader.jsx
+++ front-end/src/components/Uploader.jsx
@@ -1,22 +1,25 @@
-import { ChangeEvent } from 'react'
-import {  Button, Typography, Box } from '@mui/material';
+import { ChangeEvent, useState } from 'react'
+import { Button, Typography, Box } from '@mui/material';
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { uploadPhoto } from '../api/item/photo';
 import { UploadImageApi } from '../api/item/UploadImageApi';
 
 export const Uploader = () => {
+    const [imageError, setImageError] = useState(false);
     const queryClient = useQueryClient();
     const mutation = useMutation({
         mutationFn: UploadImageApi,
         onSuccess: (response) => {
             // queryClient.invalidateQueries({queryKey:["photos"]})
             console.log(response)
+            setImageError(false)
         },
         onError: (error) => {
             console.log(error)
+            setImageError(true)
         }
     })
-    const uploadFile  = (e) => {
+    const uploadFile = (e) => {
         const files = e.target.files
         const file = e.target.files[0]
         const formData = new FormData();
@@ -28,20 +31,44 @@ export const Uploader = () => {
 
     return (
         <>
-            <Box>
-                <Button className="aspect-ratio-container" variant="outlined-error" component="label" sx={{ height: 250, width: "100%" }}>
+            {imageError && (
+                <>
+                    <Box>
+                        <Button className="aspect-ratio-container" variant="outlined-error" component="label" sx={{ height: 250, width: "100%" }}>
 
-                    <img alt="tutorial"
+                            <img alt="tutorial"
 
-                        src="https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png">
+                                src="https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png">
 
-                    </img>
+                            </img>
 
 
-                    <input hidden accept="image/*" multiple type="file" onChange={uploadFile} />
-                </Button>
-                <Typography textAlign={"start"} fontSize={16} sx={{ color: "#d9534f", pl: 1 }}>Image is required</Typography>
-            </Box>
+                            <input hidden accept="image/*" multiple type="file" onChange={uploadFile} />
+                        </Button>
+                        <Typography textAlign={"start"} fontSize={16} sx={{ color: "#d9534f", pl: 1 }}>Image is required</Typography>
+                    </Box>
+                </>
+
+            )
+            }
+            {
+                !imageError && (
+                    <>
+                        <Button className="aspect-ratio-container" variant="outlined-striped" component="label" sx={{ height: 250, width: "100%" }}>
+
+                            <img alt="tutorial"
+
+                                src="https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png">
+
+                            </img>
+
+
+                            <input hidden accept="image/*" multiple type="file" onChange={uploadFile} />
+                        </Button>
+                    </>
+                )
+            }
+
         </>
     )
 }
