4662dd8583081d1f24735b1cb0b6894c4e0bed82
sgecowisetech <sgecowisetech@gmail.com>
2025-02-09 16:17:58 +0800

    add user auth for staff pages

--- front-end/src/ProtectedRoutes.jsx
+++ front-end/src/ProtectedRoutes.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import ProfileInformationCard from './components/user/ProfileInformationCard'
-
+import StaffDashboard from './pages/StaffDashboard'
+import StaffListItems from './pages/StaffListItems'
+import StaffAddItem from './pages/StaffAddItem'
+import StaffUpdateItem from './pages/StaffUpdateItem'
+import FindItem from './pages/FindItem'
+import ClaimScanner from './pages/ClaimScanner'
 
 const ProtectedRoutes = [
-
+    {
+        path: "staff/list/scan",
+        element: <ClaimScanner />,
+    }, 
+    {
+        path: "staff/list/add",
+        element: <StaffAddItem />,
+    },
+    {
+        path: "staff/list/update/:id",
+        element: <StaffUpdateItem />,
+    },
+    {
+        path: "find",
+        element: <FindItem />,
+    },
+    {
+        path: "/staff/list",
+        element: <StaffListItems />,
+    },
+    {
+        path: "/staff/dashboard",
+        element: <StaffDashboard />,
+    },
     {
         path: "profile/*",
         element: <ProfileInformationCard />,
--- front-end/src/PublicRoutes.jsx
+++ front-end/src/PublicRoutes.jsx
@@ -3,39 +3,11 @@ import Homepage from './pages/Homepage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import PasswordResetPage from './pages/PasswordResetPage'
-import StaffDashboard from './pages/StaffDashboard'
-import StaffListItems from './pages/StaffListItems'
-import StaffAddItem from './pages/StaffAddItem'
-import StaffUpdateItem from './pages/StaffUpdateItem'
-import FindItem from './pages/FindItem'
-import ClaimScanner from './pages/ClaimScanner'
+
 
 const PublicRoutes = [
     
-    {
-        path: "staff/list/scan",
-        element: <ClaimScanner />,
-    }, 
-    {
-        path: "staff/list/add",
-        element: <StaffAddItem />,
-    },
-    {
-        path: "staff/list/update/:id",
-        element: <StaffUpdateItem />,
-    },
-    {
-        path: "find",
-        element: <FindItem />,
-    },
-    {
-        path: "/staff/list",
-        element: <StaffListItems />,
-    },
-    {
-        path: "/staff/dashboard",
-        element: <StaffDashboard />,
-    },
+    
     {
         path: "/home",
         element: <Homepage />,
